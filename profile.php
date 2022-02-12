c95014346b52993b0ec3236b6a637b469176b549
ningbidmini <67812699+ningbidmini@users.noreply.github.com>
2022-02-12 23:09:13 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,12 @@
 
 $access_token = 'OYm/oMDP/y/rwNpxD0LqfbImJ8VzWgjkiO0LgK3bueLCMkCdipHBZxn1XZg9hN6hBZt+tqXn5O2BHeYJ+lZVQZGOvGOHIxTH4jTf8rXICcic/CsYIKxGtQBYmgi/du/cmzl3602tpyf3Pmph3ImRywdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'U1c5dc7c1232c2412eeef8c1a04d60c9a';
+if(isset($_POST['userid'])){
+  $userId = $_POST['userid'];
+}else{
+  $userId = 'U1c5dc7c1232c2412eeef8c1a04d60c9a';
+}
+
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
