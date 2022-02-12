66f9071746e84c2936c37ba3c25ec82a90533ef9
ningbidmini <67812699+ningbidmini@users.noreply.github.com>
2022-02-12 09:14:16 +0700

    Update testprofile.php

--- testprofile.php
+++ testprofile.php
@@ -74,6 +74,49 @@ $(function(){
    }, err => console.error(err.code, error.message));
   });
   
+  
+  function closeWindows() {
+         var browserName = navigator.appName;
+         var browserVer = parseInt(navigator.appVersion);
+         //alert(browserName + " : "+browserVer);
+
+         //document.getElementById("flashContent").innerHTML = "<br>&nbsp;<font face='Arial' color='blue' size='2'><b> You have been logged out of the Game. Please Close Your Browser Window.</b></font>";
+
+         if(browserName == "Microsoft Internet Explorer"){
+             var ie7 = (document.all && !window.opera && window.XMLHttpRequest) ? true : false;  
+             if (ie7)
+             {  
+               //This method is required to close a window without any prompt for IE7 & greater versions.
+               window.open('','_parent','');
+               window.close();
+             }
+            else
+             {
+               //This method is required to close a window without any prompt for IE6
+               this.focus();
+               self.opener = this;
+               self.close();
+             }
+        }else{  
+            //For NON-IE Browsers except Firefox which doesnt support Auto Close
+            try{
+                this.focus();
+                self.opener = this;
+                self.close();
+            }
+            catch(e){
+
+            }
+
+            try{
+                window.open('','_self','');
+                window.close();
+            }
+            catch(e){
+
+            }
+        }
+    }
   $('#btn_registers').on('click',function(){
     var myformdata = {
      'data_userid':$('#myform_registers #txt_userid').val(), 
@@ -98,7 +141,8 @@ $(function(){
               window.alert('Members Registers Success');
             }
             // location='./testprofile.php';
-            window.close();
+            closeWindows();
+            // window.close();
             // liff.closeWindow();
         } else {
             liff.closeWindow();
